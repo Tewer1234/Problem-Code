bd482b345d4573956286354331f1b09f9fe31579
Justin Meng <108650597+Tewer1234@users.noreply.github.com>
2023-11-16 20:49:23 +0800

    Update UVA 442 Matrix Chain Multiplication.cpp

--- UVA 442 Matrix Chain Multiplication.cpp
+++ UVA 442 Matrix Chain Multiplication.cpp
@@ -1,76 +1,56 @@
 #include <iostream>
-#include <stack>
 #include <stdlib.h>
 #include <stdio.h>
+#include <algorithm>
+#include <bits/stdc++.h>
+#include <map>
+#include <set>
+#include <stack>
+#include <queue>
+#include <iomanip>
+#include <vector>
+#define ll long long int
+#define pb push_back
+#define SIZE 400005
+#define pp pair<ll,ll>
 using namespace std;
-struct node
-{
-	int row;
-	int column;
-};
-node mat[128];
-stack <node> st;
-bool check(node a, node b)
-{
-	return a.column == b.row;
-}
-int main()
-{
-	int n, row, column, ans;
-	bool is_error;
-	char c;
-	string s;
-	cin >> n;
-	for (int i=0; i<n; i++)
-	{
-		cin >> c >> row >> column;
-		mat[c].row = row;
-		mat[c].column = column;
-	}
-	while (cin >> s)
-	{		
-		while (!st.empty()) st.pop();
-		is_error = false;
-		ans = 0;	
-		for (int i=0; i<s.size(); i++)
-		{
-			if (s[i] >= 'A' && s[i] <= 'Z')
-			{
-				st.push(mat[s[i]]);
-			} else if (s[i] == ')')
-			{
-				if (st.size() > 1)
-				{
-					cout<<st.size()<<"\n";
-					node x, y;
-					y = st.top();
-					st.pop();
-					x = st.top();
-					st.pop();
-//					cout << x.column << " " << y.row << "\n";
-					if (check(x, y))
-					{
-						ans += x.row * x.column * y.column;
-						node res;
-						res = {x.row, y.column};
-						st.push(res);
-					} else
-					{
-						is_error = true;
-						break;
-					}	
-				} 
-			}
-		}
-		
-		if (is_error)
-		{
-			cout << "error\n";
-		} else
-		{
-			cout << ans << "\n";
-		}
-	}
-	
-	return 0;
+pp ary[SIZE];
+int main() {
+  int n;
+  char c;
+  cin>>n;
+  while (n--){
+    cin>>c>>ary[c].first>>ary[c].second;
+  }
+  
+  string s;
+  while (cin>>s){
+    stack <pp> st;
+    int flag=1;
+    ll ans=0;
+    if (s.size()==1) cout<<"0\n";
+    else{
+      for (int i=0;i<s.size();i++){
+        if (s[i]==')'){
+          pp bottom=st.top();
+          st.pop();
+          pp top=st.top();
+          st.pop();
+          if (top.second!=bottom.first){
+            flag=0;
+            break;
+          }else{
+            ans+=(top.first*top.second*bottom.second);
+            st.push(make_pair(top.first,bottom.second));
+          }
+        }else if (s[i]>='A' and s[i]<='Z'){
+          st.push(ary[s[i]]);
+        }
+      }
+      
+      if (!flag) cout<<"error\n";
+      else cout<<ans<<'\n';
+    }
+  }
+  return 0;
 }
